49c540905737f20837d58398ac13c40847f0d9e2
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-05 11:58:56 +0530

    configured customer utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ module.exports = {
   darkMode: "selector",
   content: ["./*.{html,js,jsx}", "./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        red: "2px",
+      },
+      colors: {
+        "red-border": "#ef4444",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".red": {
+          "border-width": "2px",
+          "border-color": "#ef4444",
+        },
+      });
+    },
+  ],
 };
