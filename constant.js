566a51e88e77ef6bf874d220e653b3186663e77a
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-05 15:14:16 +0530

    added constants

---
+++ constant.js
@@ -0,0 +1,11 @@
+export const URL_HOME =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+export const IMG_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
+export const REST_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+const INPUT_AUTOCOMPLETE =
+  "https://www.swiggy.com/dapi/misc/place-autocomplete?input=Banglore&types=";
+const AFTER_SEARCH =
+  "https://www.swiggy.com/dapi/misc/address-recommend?place_id=ChIJbU60yXAWrjsR4E9-UejD3_g";
