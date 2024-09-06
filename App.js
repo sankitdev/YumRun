8781a73267e035b86eb8fae0f86ba37902207e04
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-07 02:33:13 +0530

    implemented react-roter-dom

--- App.js
+++ App.js
@@ -5,17 +5,43 @@ import DarkModeToggle from "./src/Components/ToggleDark";
 import Body from "./src/Components/Body";
 import NavBar from "./src/Components/NavBar";
 import Footer from "./src/Components/Footer";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Search from "./src/Components/Search";
+import Error from "./src/Components/Error";
+import { Outlet } from "react-router-dom";
+import RestaurantMenu from "./src/Components/RestaurantMenu";
 const App = () => {
   return (
     <div className="w-full min-h-screen bg-slate-100 dark:bg-black ">
       <NavBar />
-      <Body />
+      <Outlet />
       <DarkModeToggle />
       <Footer />
     </div>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/search",
+        element: <Search />,
+      },
+      {
+        path: "/restaurant/:id",
+        element: <RestaurantMenu />,
+      },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
