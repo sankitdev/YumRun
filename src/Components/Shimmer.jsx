fd48b78b5307381601ecb214477e050ced2e3d2d
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-09 20:24:26 +0530

    shimmer UI

--- src/Components/Shimmer.jsx
+++ src/Components/Shimmer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Shimmer() {
   return (
-    <div className="animate-pulse w-72 h-60 rounded-lg overflow-hidden bg-gray-200 dark:bg-gray-800">
+    <div className="animate-pulse my-4 w-44 md:w-64 lg:w-72 h-60 rounded-lg overflow-hidden bg-gray-200 dark:bg-gray-800">
       <div className="h-3/5 w-full bg-gray-300 dark:bg-gray-700"></div>
       <div className="p-2 space-y-2">
         <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded"></div>
