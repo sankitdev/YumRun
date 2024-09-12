7b52dc5a4b10cfc94c1bee4bd781d97870af4e5f
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-12 15:47:20 +0530

    added strict mode

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import DarkModeToggle from "./Components/ToggleDark";
-import Body from "./Components/Body";
+import Body from "./Components/Home/Body";
 import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Search from "./Components/Search";
 import Error from "./Components/Error";
 import { Outlet } from "react-router-dom";
-import RestaurantMenu from "./Components/RestaurantMenu";
+import RestaurantMenu from "./Components/MenuSection/RestaurantMenu";
 import Cart from "./Components/Cart"
 const App = () => {
   return (
-    <div className="w-full min-h-screen bg-white dark:bg-black ">
+    <div className="w-full min-h-screen bg-white dark:bg-slate-800 dark:text-white ">
       <NavBar />
       <Outlet />
       <DarkModeToggle />
@@ -49,4 +49,4 @@ const appRouter = createBrowserRouter([
 ]);
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(<RouterProvider router={appRouter} />);
+root.render(<StrictMode><RouterProvider router={appRouter}/></StrictMode> );
