5b9564f6f6907c950e6e3918a0ab2d94a5dc15f8
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-13 18:16:03 +0530

    organized into hook folder

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDEUk7XrdWz6bH5qiYcBcIOfoxTxQqTCyg",
+  authDomain: "yum-run-d1979.firebaseapp.com",
+  projectId: "yum-run-d1979",
+  storageBucket: "yum-run-d1979.appspot.com",
+  messagingSenderId: "1071011718906",
+  appId: "1:1071011718906:web:88cf0989822445a687cb43"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
