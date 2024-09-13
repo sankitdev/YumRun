444452f5a11ca484efe54c50c509ce2ffc21e1b9
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-13 18:17:10 +0530

    added dispatch action redux

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,6 +1,23 @@
-import BannerShimmer from "./Shimmer/BannerShimmer"
-
+import { useDispatch, useSelector } from "react-redux"
+import FoodItems from "./FoodItems"
+import { clearCart } from "../utils/cartSlice.js"
 const Cart = ()=>{
-  return <h1>cart</h1>
+  const cartItems = useSelector((store)=>store.cart.items)
+  const dispatch = useDispatch()
+  const handleClear = () =>{
+    dispatch(clearCart())
+  }
+  return (
+    <div className="px-10 my-4">
+      <h1 className="text-xl font-semibold my-4 text-center">Total Cart Items: {cartItems.length} </h1>
+     <div className="flex gap-5 flex-wrap justify-center items-center">
+     {cartItems.map(items=>(
+        <FoodItems key={items.id} {...items}/>
+      ))}
+     </div>
+     {/* <button className="text-white px-2 py-1 rounded-md bg-red-500 font-semibold hover:bg-red-600 hover:scale-105 transition-all duration-300" onClick={handleClear}>Clear Cart</button> */}
+    </div>
+  
+)
 }
 export default Cart
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,20 +1,33 @@
 import React from "react";
+import { CiShoppingCart, CiSearch } from "react-icons/ci";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 const NavBar = () => {
+  const cartItems = useSelector((store) => store.cart.items)
+  // console.log(cartItems)
   return (
-    <div className="dark:text-white dark:bg-inherit bg-white flex justify-between py-2 px-10 md:px-24 lg:px-24 h-16 items-center sticky top-0 z-10">
+    <div className="bg-white dark:bg-inherit flex justify-between py-2 px-10 md:px-24 lg:px-24 h-16 items-center sticky top-0 z-10">
       <h1 className="font-bold text-xl font-custom text-orange-500">
-        <Link to={"/"}>Yum<span className="text-green-500">Run</span></Link>
+        <Link to={"/"}>
+          Yum<span className="text-green-500">Run</span>
+        </Link>
       </h1>
-      <ul className="flex gap-4 text-lg font-medium">
+      <ul className="flex gap-7 text-lg font-medium">
         <li className="hover:text-orange-500 transition-all duration-300 ease-in-out ">
           <Link to="/">Home</Link>
         </li>
 
-        <li className="hover:text-orange-500 transition-all duration-300 ease-in-out ">
-          <Link to="/search">Search</Link>
+        <li className="hover:text-orange-500 transition-all duration-300 ease-in-out">
+          <Link to="/search" className="flex items-center space-x-1"> <span> Search</span> <CiSearch className="text-xl " /></Link>
         </li>
-        <li className="hover:text-orange-500 transition-all duration-300 ease-in-out "><Link to="/cart">Cart</Link></li>
+        <li className="relative hover:text-orange-500 transition-all duration-300 ease-in-out">
+          <Link to="/cart">
+            <CiShoppingCart className="text-3xl" />
+            <span className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+              {cartItems.length}
+            </span>
+          </Link>
+        </li>
       </ul>
     </div>
   );
