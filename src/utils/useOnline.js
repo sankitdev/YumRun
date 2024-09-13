5215df34a017254df1e3de0b3684086747912686
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-13 09:07:29 +0530

    addded cleanup function to unmount event listener

--- src/utils/useOnline.js
+++ src/utils/useOnline.js
@@ -2,13 +2,22 @@ import { useState,useEffect } from "react";
 
 const useOnline = ()=>{
     const [isOnline, setIsOnline] = useState(navigator.onLine)
+    const handleOnline = ()=>{
+        console.log("IsOnline is here")
+        setIsOnline(true)
+    }
+    const handleOffline = ()=>{
+        console.log("IsOffline is here")
+        setIsOnline(false)
+    }
     useEffect(()=>{
-        window.addEventListener("online",()=>{
-            setIsOnline(true)
-        })
-        window.addEventListener("offline",()=>{
-            setIsOnline(false)
-        })
+        window.addEventListener("online",handleOnline)
+        window.addEventListener("offline",handleOffline)
+        return ()=>{
+            console.log("Cleaning the eventListener")
+            window.removeEventListener("online",handleOnline)
+            window.removeEventListener("offline",handleOffline)
+        }
     },[])
     return isOnline
 }
