d1f7ebed1965d2f5901ed3509f9012616946f9b8
Ankit Singh <ankitsingh635101@gmail.com>
2024-10-16 08:22:34 +0530

    refactor: Add the shimmer and updating it with real data

--- src/Components/MenuSection/MenuCards.jsx
+++ src/Components/MenuSection/MenuCards.jsx
@@ -1,49 +1,83 @@
 import { IMG_URL } from "../../constant";
-import icon from '../../assets/star.svg'
+import icon from "../../assets/star.svg";
 import { addItem } from "../../store/cartSlice";
 import { useDispatch } from "react-redux";
+import { useState } from "react";
 const MenuCard = ({ items }) => {
-    const truncateDescription = (description, maxLength) => {
-        if (description?.length > maxLength) {
-          return `${description.slice(0, maxLength)}...more`;
-        }
-        return description;
-      };
-      const dispatch = useDispatch()
-      const handleAddItem = (info)=>{
-        dispatch(addItem(info));
-      }
+  const [addedItems, setAddedItems] = useState({});
+  const truncateDescription = (description, maxLength) => {
+    if (description?.length > maxLength) {
+      return `${description.slice(0, maxLength)}...more`;
+    }
+    return description;
+  };
+  const dispatch = useDispatch();
+  const handleAddItem = (info) => {
+    dispatch(addItem(info));
+    setAddedItems((prevAddedItems) => ({
+      ...prevAddedItems,
+      [info.id]: true,
+    }));
+  };
   return (
     <>
-      {items && Array.isArray(items) ? (items?.slice(1)?.map((rescard, index) => {
-        const {card} = rescard?.card || {}
-        const {title, itemCards} = card || {}
-              return ( <div key={index} className="w-full md:w-1/2 lg:w-1/2">
+      {items && Array.isArray(items)
+        ? items?.slice(1)?.map((rescard, index) => {
+            const { card } = rescard?.card || {};
+            const { title, itemCards } = card || {};
+            return (
+              <div key={index} className="w-full md:w-1/2 lg:w-1/2">
                 <h1 className="text-xl font-bold">{title}</h1>
-                {itemCards?.map((item) =>{
-                  const{info}= item?.card || {}
-                  const{id,name,price,ratings,description,imageId} = info
+                {itemCards?.map((item) => {
+                  const { info } = item?.card || {};
+                  const { id, name, price, ratings, description, imageId } =
+                    info;
                   return (
-                    <div key={id} className="flex rounded-md p-2 my-2 w-full shadow-md dark:shadow-slate-700">
-                    <div className="w-4/5">
-                      <h1 className="text-lg font-semibold">{name}</h1>
-                      <p className="pb-1">
-                        <span className="font-semibold">₹{Math.round(price / 100) ||  "150"} </span> <span></span>
-                      </p>
-                      <p className="flex"><img src={icon} alt=""/> <span className="text-green-600 font-medium">{ratings?.aggregatedRating?.rating}</span></p>
-                      <p className="leading-4 text-gray-600 dark:text-gray-300">{truncateDescription(description, 100)}</p>
+                    <div
+                      key={id}
+                      className="flex rounded-md p-2 my-2 w-full shadow-md dark:shadow-slate-700"
+                    >
+                      <div className="w-4/5">
+                        <h1 className="text-lg font-semibold">{name}</h1>
+                        <p className="pb-1">
+                          <span className="font-semibold">
+                            ₹{Math.round(price / 100) || "150"}{" "}
+                          </span>{" "}
+                          <span></span>
+                        </p>
+                        <p className="flex">
+                          <img src={icon} alt="" />{" "}
+                          <span className="text-green-600 font-medium">
+                            {ratings?.aggregatedRating?.rating}
+                          </span>
+                        </p>
+                        <p className="leading-4 text-gray-600 dark:text-gray-300">
+                          {truncateDescription(description, 100)}
+                        </p>
+                      </div>
+                      <div className="text-center h-40 overflow-hidden rounded-lg shadow-md">
+                        <img
+                          className="w-44 h-3/4 object-cover object-center hover:scale-105 transition-all duration-150"
+                          src={`${IMG_URL}${imageId}`}
+                          alt=""
+                        />
+                        <button
+                          className="w-2/3 py-1 border-2 border-grey dark:border-white font-bold text-green-500 rounded-md hover:bg-slate-200"
+                          onClick={() => handleAddItem(info)}
+                          disabled={addedItems[id]}
+                        >
+                          {addedItems[id] ? "Added" : "Add"}
+                        </button>
+                      </div>
                     </div>
-                    <div className="text-center h-40 overflow-hidden rounded-lg shadow-md">
-                      <img className="w-44 h-3/4 object-cover object-center hover:scale-105 transition-all duration-150" src={`${IMG_URL}${imageId}`} alt="" />
-                      <button className="w-2/3 py-1 border-2 border-grey dark:border-white font-bold text-green-500 rounded-md hover:bg-slate-200" onClick={()=>handleAddItem(info)}>Add</button>
-                    </div>
-                  </div>
-                  )
-                } )}
-        </div>)
-      })):"Loading...."}
+                  );
+                })}
+              </div>
+            );
+          })
+        : "Loading...."}
     </>
   );
-}
+};
 
 export default MenuCard;
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,25 +1,25 @@
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 // import useFetchMenu from "../utils/useFetchMenu"
-import {PRE_SEARCH} from "../constant.js"
+import { PRE_SEARCH } from "../constant.js";
 import Banner from "./Home/Banner.jsx";
 const Search = () => {
   const [text, setText] = useState("");
-  const [data,setData] = useState(null)
-  useEffect(()=>{
-    const fetchSearch = async ()=>{
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchSearch = async () => {
       try {
-        const response = await axios.get(PRE_SEARCH)
+        const response = await axios.get(PRE_SEARCH);
         // console.log(response)
-       console.log(Object.values(response.data.data.cards))
-      setData(Object.values(response.data.data.cards))
+        console.log(Object.values(response.data.data.cards));
+        setData(Object.values(response.data.data.cards));
       } catch (error) {
-        console.error("Error in Search Component",error)
-        return <p>Error Finding Data</p>
+        console.error("Error in Search Component", error);
+        return <p>Error Finding Data</p>;
       }
-    }
-    fetchSearch()
-  },[])
+    };
+    fetchSearch();
+  }, []);
 
   return (
     <div className="flex flex-col justify-center items-center my-10 px-10">
@@ -31,9 +31,12 @@ const Search = () => {
           placeholder="Search..."
           onChange={(e) => setText(e.target.value)}
         />
-        <Banner title={data?.[1]?.card?.card?.header?.title} gridList={data?.[1]?.card?.card?.imageGridCards.info} className = "gap-5 h-36"/>
+        <Banner
+          title={data?.[1]?.card?.card?.header?.title}
+          gridList={data?.[1]?.card?.card?.imageGridCards.info}
+          className="gap-5 h-36"
+        />
       </div>
-      
     </div>
   );
 };
